50e83443d90515e1934003949762624f0d709a1d
Ashutosh Chauhan <ashutoshchauhan13@gmail.com>
2019-11-18 00:48:02 +0000

    added detail for Left and Right  rotation command class

--- src/com/staunch/command/LeftRotationCommand.java
+++ src/com/staunch/command/LeftRotationCommand.java
@@ -9,10 +9,31 @@ public class LeftRotationCommand implements Command {
     }
 
     public String tellMeHowYouOperate() {
-        return "";
+        String message = "I make anti-clock wise turn";
+        System.out.println(message);
+        return message;
     }
 
     public RobotPosition processCommand(RobotPosition robotPosition) {
-        return null;
+        System.out.print("Let's Turn Anti-Clockwise");
+        char newOrientation;
+        switch (robotPosition.getOrientation()) {
+            case 'N':
+                newOrientation = 'W';
+                break;
+            case 'E':
+                newOrientation = 'N';
+                break;
+            case 'S':
+                newOrientation = 'E';
+                break;
+            case 'W':
+                newOrientation = 'S';
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + robotPosition.getOrientation());
+        }
+        robotPosition.setOrientation(newOrientation);
+        return robotPosition;
     }
 }
--- src/com/staunch/command/RightRotationCommand.java
+++ src/com/staunch/command/RightRotationCommand.java
@@ -9,10 +9,31 @@ public class RightRotationCommand implements Command {
     }
 
     public String tellMeHowYouOperate() {
-        return "";
+        String message = "I make clock wise turn";
+        System.out.println(message);
+        return message;
     }
 
     public RobotPosition processCommand(RobotPosition robotPosition) {
-        return null;
+        System.out.print("Let's Turn Clockwise");
+        char newOrientation;
+        switch (robotPosition.getOrientation()) {
+            case 'N':
+                newOrientation = 'E';
+                break;
+            case 'E':
+                newOrientation = 'S';
+                break;
+            case 'S':
+                newOrientation = 'W';
+                break;
+            case 'W':
+                newOrientation = 'N';
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + robotPosition.getOrientation());
+        }
+        robotPosition.setOrientation(newOrientation);
+        return robotPosition;
     }
 }
